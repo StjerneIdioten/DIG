40576c8f2baac6f1420fbc263b86633442f1211e
StjerneIdioten <dragonest7@gmail.com>
2016-05-11 10:27:34 +0200

    Projekt

---
+++ Portefolie2/Design.vhd
@@ -0,0 +1,42 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    09:45:31 05/11/2016 
+-- Design Name: 
+-- Module Name:    Design - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity Design is
+    Port ( Clk : in  STD_LOGIC);
+end Design;
+
+architecture Behavioral of Design is
+
+begin
+
+
+end Behavioral;
+
